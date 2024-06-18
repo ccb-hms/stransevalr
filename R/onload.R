7d08eb917dbd554d0cbc461b3b19b6318586ac34
Andrew Ghazi <6763470+andrewGhazi@users.noreply.github.com>
2024-06-18 11:30:36 -0400

    infra

--- R/onload.R
+++ R/onload.R
@@ -2,6 +2,7 @@ pathlib <- NULL
 polars <- NULL
 torch <- NULL
 transformers <- NULL
+cudapython <- NULL
 numpy <- NULL
 random <- NULL
 sentence_transformers <- NULL
@@ -11,6 +12,7 @@ sentence_transformers <- NULL
     polars <<- reticulate::import("polars", delay_load = TRUE)
     torch <<- reticulate::import("torch", delay_load = TRUE)
     transformers <<- reticulate::import("transformers", delay_load = TRUE)
+    cudapython <<- reticulate::import('cuda-python', delay_load = TRUE)
     numpy <<- reticulate::import("numpy", delay_load = TRUE)
     random <<- reticulate::import("random", delay_load = TRUE)
     sentence_transformers <<- reticulate::import("sentence_transformers", delay_load = TRUE)
