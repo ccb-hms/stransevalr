2a4d0805b5d5efd707a1f7ccab8b21f1a6458c75
Andrew Ghazi <6763470+andrewGhazi@users.noreply.github.com>
2024-06-18 22:43:21 -0400

    embed into R object complete

--- R/onload.R
+++ R/onload.R
@@ -8,16 +8,17 @@ random <- NULL
 sentence_transformers <- NULL
 
 .onLoad = function(libname, pkgname) {
-    pathlib <<- reticulate::import("pathlib", delay_load = TRUE)
-    polars <<- reticulate::import("polars", delay_load = TRUE)
-    torch <<- reticulate::import("torch", delay_load = TRUE)
-    transformers <<- reticulate::import("transformers", delay_load = TRUE)
-    cudapython <<- reticulate::import('cuda-python', delay_load = TRUE)
-    numpy <<- reticulate::import("numpy", delay_load = TRUE)
-    random <<- reticulate::import("random", delay_load = TRUE)
-    sentence_transformers <<- reticulate::import("sentence_transformers", delay_load = TRUE)
+  pathlib <<- reticulate::import("pathlib", delay_load = TRUE)
+  # polars <<- reticulate::import("polars", delay_load = TRUE)
+  pandas <<- reticulate::import("pandas")
+  torch <<- reticulate::import("torch", delay_load = TRUE)
+  transformers <<- reticulate::import("transformers", delay_load = TRUE)
+  cudapython <<- reticulate::import('cuda-python', delay_load = TRUE)
+  numpy <<- reticulate::import("numpy", delay_load = TRUE)
+  random <<- reticulate::import("random", delay_load = TRUE)
+  sentence_transformers <<- reticulate::import("sentence_transformers", delay_load = TRUE)
 }
 
 .onAttach = function(libname, pkgname) {
-    cli::cli_alert_success('stransevalr')
+  cli::cli_alert_success('stransevalr')
 }
--- R/stransevalr.R
+++ R/stransevalr.R
@@ -7,78 +7,55 @@ tmp_dir_helper = function(tmp_dir, call = rlang::caller_env()) {
     tmp_dir
 }
 
-run_sent_trans = function(scramble_dt, tmp_dir) {
+safely_check_for_gpu = function(x, call = rlang::caller_env()) {
+  check_res = tryCatch({
+    torch$cuda$get_device_name(0)
+  }, error = function(e) {
+    -1L
+  })
+  
+  if (check_res == -1) cli::cli_abort("Couldn't find a GPU, aborting. Python error: {py_last_error()}", call = call)
+  
+  check_res
+}
+
+run_sent_trans = function(scramble_dt, model_name, tmp_dir) {
+    # use_virtualenv("~/pyenv/sntenv")
     # devtools::load_all()
     # options(datatable.print.trunc.cols = TRUE)
     # tmp_dir = NULL
-    #
+    # 
     # cleanup = is.null(tmp_dir)
-    #
+    # 
     # tmp_dir = tmp_dir_helper(tmp_dir)
-    #
+    # input = "~/bioc_ai/data/correct_fmt.tsv"
+    # model_name = 'multi-qa-MiniLM-L6-cos-v1'
     # scramble_dt = input |>
     #     read_input() |>
     #     multi_scramble(tmp_dir = tmp_dir)
 
-
-    device = torch.device("cuda")
-
-    torch.cuda.get_device_name(0)
-
-    model = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
-    model2 = SentenceTransformer('pritamdeka/S-PubMedBert-MS-MARCO')
-
-    q_df = pl.read_csv('~/ai_snt/qmd/sent_trans_eval/data/gpt4_with_scramble_demo.csv', separator = "\t")
-    q_df2 = pl.read_csv('~/ai_snt/qmd/sent_trans_eval/data/llama_bioc_qa.csv')
-    q_df.select(pl.col("Question", "Response"))
-
-    comb_df = pl.concat([q_df, q_df2.select(pl.col("Response_llama2_Bioc_RAG", "Response_llama2_Temp0"))], how = "horizontal")
-
-    questions = q_df['Question'].to_list()
-    answers = q_df['Response'].to_list()
-
-    grd_embed = model.encode(answers)
-    grd_embed2 = model2.encode(answers)
-    grd_embed.shape
-
-    pl.from_numpy(grd_embed).write_csv("~/ai_snt/qmd/sent_trans_eval/output/ground_answer_embed_demo.csv")
-    pl.from_numpy(grd_embed2).write_csv("~/ai_snt/qmd/sent_trans_eval/output/m2/ground_answer_embed_m2_demo.csv")
-
-    df = pl.DataFrame(
-        {
-            "models": ["Response_Azure_GPT4_Temp0",
-                       "Response_Azure_Bioc_RAG",
-                       "Response_llama2_Bioc_RAG",
-                       "Response_llama2_Temp0",
-                       "scrambled_ground_truth",
-                       "scrambled_mixed_ground_truth",
-                       "scrabble_match_nword",
-                       "scrabble_match_nchar",
-                       "reembed_ground_truth"]
-        }
-    )
-
-    print(df)
-
-    df.write_csv("~/ai_snt/qmd/sent_trans_eval/output/model_df_demo.csv")
-
-    for idx, query in enumerate(questions):
-
-        ans_list = [comb_df.select(pl.col("Response_Azure_GPT4_Temp0")).item(idx,0),
-                    comb_df.select(pl.col("Response_Azure_Bioc_RAG")).item(idx,0),
-                    comb_df.select(pl.col("Response_llama2_Bioc_RAG")).item(idx,0),
-                    comb_df.select(pl.col("Response_llama2_Temp0")).item(idx,0),
-                    comb_df.select(pl.col("scrambled_ground_truth")).item(idx,0),
-                    comb_df.select(pl.col("scrambled_mixed_ground_truth")).item(idx,0),
-                    comb_df.select(pl.col("scrabble_match_nword")).item(idx,0),
-                    comb_df.select(pl.col("scrabble_match_nchar")).item(idx,0),
-                    comb_df.select(pl.col("reembed_ground_truth")).item(idx,0)]
-
-    query_embedding = model.encode(ans_list, convert_to_tensor=True).cpu().numpy()
-    m2_emb = model2.encode(ans_list, convert_to_tensor=True).cpu().numpy()
-
-    pl.from_numpy(query_embedding).write_csv("~/ai_snt/qmd/sent_trans_eval/output/" + str(idx) + "_demo.csv")
-    pl.from_numpy(m2_emb).write_csv("~/ai_snt/qmd/sent_trans_eval/output/m2/" + str(idx) + "_m2_demo.csv")
+    dev_name = safely_check_for_gpu(0)
+    
+    device = torch$device("cuda")
+    
+    model = sentence_transformers$SentenceTransformer(model_name)
+    
+    scramble_pd = r_to_py(scramble_dt)
+    
+    questions = scramble_pd$question$tolist()
+    answers   = scramble_pd$answer$tolist()
+    
+    grd_embed = model$encode(answers) # This is an _R_ matrix, not a python result 
+    # TODO: write this out
+    
+    to_embed = colnames(scramble_dt) |> tail(-2) # TODO: write this out
+    
+    emb_dt = data.table(i = seq_along(to_embed),
+                        m = to_embed,
+                        res = lapply(seq_along(to_embed),
+                                     \(i) data.table(model$encode(scramble_pd[[to_embed[i]]]$tolist()))))
+    
+    emb_dt[,rbindlist(res), by = .(i,m)]
 
 
 }
@@ -106,7 +83,7 @@ stransevalr = function(input,
     res = input |>
         read_input() |>
         multi_scramble(tmp_dir = tmp_dir) |>
-        run_sent_trans(sent_trans_model) |>
+        run_sent_trans(sent_trans_model, tmp_dir) |>
         eval_cs()
 
     if (cleanup) fs::dir_delete(tmp_dir)
